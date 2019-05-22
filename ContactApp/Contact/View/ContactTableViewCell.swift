f4a4b39f0ef2aaa91636b2e38d7fa03a31dd81af
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2019-05-22 17:40:15 -0300

    Some styles

--- ContactApp/Contact/View/ContactTableViewCell.swift
+++ ContactApp/Contact/View/ContactTableViewCell.swift
@@ -12,11 +12,17 @@ class ContactTableViewCell: UITableViewCell {
 
     @IBOutlet weak var contactNameLabel: UILabel!
 
+    let firstNameAttributes = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue", size: 16) as Any]
+    let lastNameAttributes = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Bold", size: 16) as Any]
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     func updateCell(firstName: String, lastName: String) {
-        contactNameLabel.text = "\(firstName) \(lastName)"
+
+        let finalText = NSMutableAttributedString(string: firstName, attributes: firstNameAttributes)
+        finalText.append(NSAttributedString(string: " \(lastName)", attributes: lastNameAttributes))
+        contactNameLabel.attributedText = finalText
     }
 }
