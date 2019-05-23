ef8f638614a5a7410c0c689798529fd34559bf02
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2019-05-23 20:28:26 -0300

    Contact detail and delete

---
+++ ContactApp/Detail/Controller/DetailViewController+CollectionView.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController+CollectionView.swift
+//  ContactApp
+//
+//  Created by Marcelo José on 23/05/2019.
+//  Copyright © 2019 Marcelo José. All rights reserved.
+//
+
+import UIKit
+
+extension DetailViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return detaildata.count
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailFieldtCellIdentifier, for: indexPath) as! DetailCollectionViewCell
+        cell.updateCell(fieldName: detaildata[indexPath.item].name, fieldData: detaildata[indexPath.item].value)
+        return cell
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let availableWidth = collectionView.frame.width
+        return CGSize(width: availableWidth, height: 50)
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
--- ContactApp/Detail/Controller/DetailViewController.swift
+++ ContactApp/Detail/Controller/DetailViewController.swift
@@ -56,32 +56,3 @@ class DetailViewController: UIViewController, ContactSelectDelegate {
     @IBAction func deleteAction(_ sender: Any) {
     }
 }
-
-extension DetailViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    public func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return detaildata.count
-    }
-
-    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailFieldtCellIdentifier, for: indexPath) as! DetailCollectionViewCell
-        cell.updateCell(fieldName: detaildata[indexPath.item].name, fieldData: detaildata[indexPath.item].value)
-        return cell
-    }
-
-    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let availableWidth = collectionView.frame.width
-        return CGSize(width: availableWidth, height: 50)
-    }
-
-    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-
-    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-}
