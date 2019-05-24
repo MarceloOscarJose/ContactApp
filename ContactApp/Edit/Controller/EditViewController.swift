2754a009eedbd5fad9ac77b8f6c313d7107312bd
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2019-05-23 22:02:25 -0300

    Add and edit controller

---
+++ ContactApp/Edit/Controller/EditViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EditViewController.swift
+//  ContactApp
+//
+//  Created by Marcelo José on 23/05/2019.
+//  Copyright © 2019 Marcelo José. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupControls()
+    }
+
+    func setupControls() {
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
