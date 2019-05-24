3f62a41c0136bfc2167511bf4877bfbb73d41863
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2019-05-24 09:42:11 -0300

    Add and edit controller

--- ContactApp/Detail/Controller/DetailViewController.swift
+++ ContactApp/Detail/Controller/DetailViewController.swift
@@ -83,9 +83,6 @@ class DetailViewController: UIViewController, ContactListDelegate {
 
 extension DetailViewController: EditViewControllerDelegate {
 
-    func didAddContact() {
-    }
-
     func didSaveContact(contactData: Contact) {
         updateDetail(contactData: contactData)
 
