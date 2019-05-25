97222be83a69234e7afded8f75de00602877eed8
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2019-05-25 04:36:47 -0300

    Add and edit controller

--- ContactApp/Detail/View/DetailHeaderView.swift
+++ ContactApp/Detail/View/DetailHeaderView.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class DetailHeaderView: UIView {
 
-    let titleLabel: UILabel = {
+    lazy var containerView: UIView = {
+        let containerView = UIView()
+        containerView.clipsToBounds = true
+        return containerView
+    }()
+    lazy var titleLabel: UILabel = {
         let titleLabel = UILabel()
-        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 23)
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 25)
         titleLabel.textAlignment = .center
         titleLabel.textColor = UIColor.darkGray
         titleLabel.adjustsFontSizeToFitWidth = true
@@ -20,33 +25,46 @@ class DetailHeaderView: UIView {
     }()
 
     var initialHeight: CGFloat!
+    var currentFontSize: CGFloat = 0
 
     override init(frame: CGRect) {
-        super.init(frame: .zero)
-        setupControls()
+        super.init(frame: frame)
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 
     func updateHeader(firstName: String, lastName: String) {
         titleLabel.text = "\(firstName) \(lastName)"
+        setupControls()
+        currentFontSize = titleLabel.font.pointSize
     }
 
     override func layoutSubviews() {
         initialHeight = self.initialHeight ?? self.frame.height
-        let scaledFont = (23 * self.frame.height / initialHeight)
-        let fontSize = scaledFont < 18 ? 18 : scaledFont > 27 ? 27 : scaledFont
+        let scaledFont = (currentFontSize * self.frame.height / initialHeight)
+        let fontSize = scaledFont < 18 ? 18 : scaledFont > currentFontSize ? currentFontSize : scaledFont
         titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: fontSize)
     }
 
     func setupControls() {
         self.backgroundColor = UIColor.headerColor
-        self.addSubview(titleLabel)
 
+        containerView.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+
+        containerView.addSubview(titleLabel)
+        self.addSubview(containerView)
+
+        containerView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        containerView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.9).isActive = true
+
+        titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
     }
 }
